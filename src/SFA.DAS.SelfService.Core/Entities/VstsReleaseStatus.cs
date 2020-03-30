def979ec5507fb28cd8732958be3384d1e5b597b
Nathan Bowes <nathan.bowes@education.gov.uk>
2020-03-30 12:06:50 +0100

    Codemaid cleanup

--- src/SFA.DAS.SelfService.Core/Entities/VstsReleaseStatus.cs
+++ src/SFA.DAS.SelfService.Core/Entities/VstsReleaseStatus.cs
@@ -6,14 +6,19 @@ namespace SFA.DAS.SelfService.Core.Entities
     {
         [Description("Fetching Status")]
         Undefined = 0,
+
         [Description("Queued")]
         NotDeployed = 1,
+
         [Description("In Progress")]
         InProgress = 2,
+
         [Description("Whitelisted")]
         Succeeded = 4,
+
         [Description("Whitelisted")]
         PartiallySucceeded = 8,
+
         Failed = 16,
         All = 31
     }
