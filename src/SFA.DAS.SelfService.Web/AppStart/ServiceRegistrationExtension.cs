9a1418a4ae02049155f0e70b87e5a9f202ae9b8e
Craig Gumbley <craiggumbley@gmail.com>
2020-04-16 11:28:02 +0100

    Shared components (#17)
    * Use shared config map
    
    * Add custom ai initializer
    
    * Consume baseurl from shared config
    
    * Update app insights package
    
    * remove arm templates

--- src/SFA.DAS.SelfService.Web/AppStart/ServiceRegistrationExtension.cs
+++ src/SFA.DAS.SelfService.Web/AppStart/ServiceRegistrationExtension.cs
@@ -1,10 +1,12 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.Extensibility;
 using SFA.DAS.SelfService.Core.Configuration;
 using SFA.DAS.SelfService.Core.IReleases;
 using SFA.DAS.SelfService.Core.IServices;
 using SFA.DAS.SelfService.Core.Services;
 using SFA.DAS.SelfService.Infrastructure.Releases;
+using SFA.DAS.SelfService.Web.Infrastructure;
 
 namespace SFA.DAS.SelfService.Web.AppStart
 {
@@ -15,6 +17,7 @@ namespace SFA.DAS.SelfService.Web.AppStart
             services.AddSingleton<IReleaseService, ReleaseService>();
             services.AddSingleton<IReleaseClient, ReleaseClient>();
             services.Configure<VstsConfiguration>(configuration.GetSection("VstsConfig"));
+            services.AddSingleton<ITelemetryInitializer, ToolsTelemetryInitializer>();
         }
     }
 }
